6e3052f9d0bb7085dd503f7678a33b67a692450e
Brenda Yañez <brendayw97@gmail.com>
2024-09-25 18:26:03 -0300

    actualizo rutas()

--- models/server.js
+++ models/server.js
@@ -14,6 +14,7 @@ class Server {
 
   rutas () {
     this.app.use('/api/v1/dogs', require('../routes/dogs')) // Miqueas Correa
+    this.app.use('/api/v1/peces', require('../routes/peces')) // Brenda Yañez
   }
 
   listen () {
