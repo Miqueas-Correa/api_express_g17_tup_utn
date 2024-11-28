db0f8b5e4a22cd50a6d25e2f9216229184db1818
Miqueas Correa <miki.mate.tizi@gmail.com>
2024-11-28 19:31:16 -0300

    agrego CORS

--- models/server.js
+++ models/server.js
@@ -1,28 +1,35 @@
 const express = require('express')
+const cors = require('cors') // Importamos cors
 
 class Server {
   constructor () {
     this.app = express()
     this.port = process.env.PORT || 3000
-    this.middleware()
-    this.rutas()
+    this.middleware() // Llama al middleware
+    this.rutas() // Establece las rutas
   }
 
   middleware () {
-    this.app.use(express.static('public'))
+    // Habilitamos CORS globalmente para todas las rutas
+    this.app.use(cors({
+      origin: '*', // Permite solicitudes de todos los orígenes
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+      allowedHeaders: ['Content-Type', 'Authorization'] // Headers permitidos
+    }))
+
+    this.app.use(express.static('public')) // Sirve archivos estáticos desde la carpeta 'public'
   }
 
   rutas () {
-    this.app.use('/api/v1/dogs', require('../routes/dogs')) // Miqueas Correa
-    // Establece el punto de entrada para las rutas de cocodrilos, todas las solicitudes que comienzan con '/api/v1/crocodiles' serán gestionadas por el archivo de rutas '../routes/crocodiles'.
-    this.app.use('/api/v1/crocodiles', require('../routes/crocodiles')) // Matias Emanuel Coronel Dittler
-    this.app.use('/api/v1/cats', require('../routes/cats')) // Wanda Suarez
-    this.app.use('/api/v1/peces', require('../routes/peces')) // Brenda Yañez
+    this.app.use('/api/v1/dogs', require('../routes/dogs')) // Rutas de perros
+    this.app.use('/api/v1/crocodiles', require('../routes/crocodiles')) // Rutas de cocodrilos
+    this.app.use('/api/v1/cats', require('../routes/cats')) // Rutas de gatos
+    this.app.use('/api/v1/peces', require('../routes/peces')) // Rutas de peces
   }
 
   listen () {
     this.app.listen(this.port, () => {
-      console.log(`La API esta escuchando en el this.PORT ${this.port}`)
+      console.log(`La API está escuchando en el puerto ${this.port}`)
     })
   }
 }
