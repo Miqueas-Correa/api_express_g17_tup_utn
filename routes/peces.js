f2f9f6ed52d7f3e0b78078f133b885de6fad0085
Brenda Yañez <brendayw97@gmail.com>
2024-12-01 13:50:11 -0300

    cambio final de ruta

--- routes/peces.js
+++ routes/peces.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
-const { getPeces, getPezByColor, getPezByEspecie, getPezByNombre, getPezByID } = require('../controllers/peces')
+const { getPeces, getPez, /*getPezByColor*/ getPezByEspecie, getPezByNombre, /*getPezByID*/ } = require('../controllers/peces')
 
 const rutas = Router()
 
 rutas.get('/', getPeces)
-rutas.get('/:colorPez', getPezByColor)
+rutas.get('/:id', getPez)
+// rutas.get('/:colorPez', getPezByColor)
 rutas.get('/especie/:especiePez', getPezByEspecie)
 rutas.get('/nombre/:nombrePez', getPezByNombre)
-rutas.get('/id/:idPez', getPezByID)
+// rutas.get('/id/:idPez', getPezByID)
 
 module.exports = rutas
